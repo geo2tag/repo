d4dd2b303ff0c8e2078147301294b84e428539f7
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-01-17 11:44:54 +0000

    added debug messages

--- import_packages.sh
+++ import_packages.sh
@@ -3,12 +3,21 @@
 INCOMING=`pwd`/incoming
 DISTR=testing
 
+
+echo "Running import on `hostname`"
+echo "incoling=${INCOMING}"
+echo "= Incoming content is="
+ls -la ${INCOMING}
+echo "### State before update: ###"
+reprepo list 
+
 #
 # Make sure we're in the apt/ directory
 #
 cd $INCOMING
 cd ..
 
+echo ${INCOMING}
 #
 #  See if we found any new packages
 #
@@ -31,10 +40,13 @@ fi
 #
 #  Now import each new package that we *did* find
 #
-for i in $INCOMING/*.changes; do
+for i in $INCOMING/*.deb; do
+
+echo $i
 
   # Import package to 'sarge' distribution.
-  reprepro  --ignore=wrongdistribution  -Vb . include ${DISTR} $i
+  reprepro  --ignore=wrongdistribution  -Vb . includedeb ${DISTR} $i
+	echo $i
 
   # Delete the referenced files
   sed '1,/Files:/d' $i | sed '/BEGIN PGP SIGNATURE/,$d' \
@@ -48,10 +60,50 @@ for i in $INCOMING/*.changes; do
       #  Delete the referenced file
       #
       if [ -f "$INCOMING/$NAME" ]; then
-          rm "$INCOMING/$NAME"  || exit 1
+       #   rm "$INCOMING/$NAME"  || exit 1
+	echo  "$INCOMING/$NAME"  || exit 1
       fi
   done
 
   # Finally delete the .changes file itself.
-  rm  $i
+#  rm  $i
 done
+
+
+
+
+#
+#  Now import each new package that we *did* find
+#
+for i in $INCOMING/*.changes; do
+
+echo $i
+
+  # Import package to 'sarge' distribution.
+  reprepro  --ignore=wrongdistribution  -Vb . include ${DISTR} $i
+	echo $i
+
+  # Delete the referenced files
+  sed '1,/Files:/d' $i | sed '/BEGIN PGP SIGNATURE/,$d' \
+       | while read MD SIZE SECTION PRIORITY NAME; do
+        
+      if [ -z "$NAME" ]; then
+           continue
+      fi
+
+      #
+      #  Delete the referenced file
+      #
+      if [ -f "$INCOMING/$NAME" ]; then
+       #   rm "$INCOMING/$NAME"  || exit 1
+	echo  "$INCOMING/$NAME"  || exit 1
+      fi
+  done
+
+  # Finally delete the .changes file itself.
+#  rm  $i
+done
+
+
+echo "### State after update: ###"
+reprepo list 
